b3c5a3e245c19c78c1de1fd2c322bc85179c322d
lebendig <lebendig@hotmail.com>
2024-10-03 17:03:55 +0200

    add new drinks

--- src/app/service/drink.service.ts
+++ src/app/service/drink.service.ts
@@ -48,12 +48,16 @@ export class DrinkService {
     { name: '43 Sour', recipe: '43 Likör 6 cl, Aquafaba 6 cl (2 Schuss), 1 Zitrone (1.5 se nao tiver muito suco), Muito pouquinho Gommesirup (metade de um Schuss 1 cl)' },
     { name: 'Negroni', recipe: 'Campari 3cl, Gin 3cl, Red Vermouth 3cl, Orangenschale 3 - 4 Stück, Gerührt (großer Eisblock)' },
     { name: 'Purple Negroni', recipe: 'Purple Gin 4cl, Italicus 3cl, Dry Vermouth 3cl, Limettenschale 2 - 3 Stück, Gerührt (großer Eisblock), Deco: Limmetenschale' },
-    { name: 'Purple Rain', recipe: 'Purple Gin 6cl, 1 garrafa de agua amarga, meio limao expremido no copinho, Deco: dourado, coisinha verde e um palito' },
+    { name: 'Purple Rain', recipe: 'Purple Gin 6cl, 1 Henry Tonic, meio limao expremido no copinho, Deco: dourado, coisinha verde e um palito' },
     { name: 'Mexican Negroni', recipe: 'Mezcal cl, um que Fica atras do Mezcal 3cl, Red Vermouth 3cl, Orangenschale 2 - 3 Stück, Gerührt (großer Eisblock), Deco: Limmetenschale' },
     { name: 'Don Martinez', recipe: 'Tequila 5cl, Mezcal 1cl, Ancho verde 1cl, Vermouth White 3cl, Gerührt (großer Eisblock), Deco: Limmetenschale' },
     { name: 'The Artist', recipe: 'Purple Gin 6cl, Gurkenschale 2-3 Stück, Orangenschale 2-3 Stück, 0.5 Zitrone (1 se nao tiver muito suco), Gommesirup (2 Schus oder 4 cl), Glas vorkühlen mit Crushed ice, Coloca espuma no copo 1/3 do copo, decora a vontade com chispinhas' },
     { name: 'Ginger Highball', recipe: 'Red Label/Jack Daniels 6cl, 1 Halbe Limette, Gommesirup (2 Schus oder 4 cl), Ingwer fein geschnitten, Ginger Ale (Thomas Henry), Mistura no copo, no precisa shakear' },
     { name: 'Mezcal Highball', recipe: 'Mezcal 4cl, Olympia 1cl, White Vermouth 2cl, 1/2 Orange, Spicy Ginger (Thomas Henry), Mistura no copo, no precisa shakear' },
+    { name: 'Primitivo', recipe: 'Wein' },
+    { name: 'Sauvignon Touraine', recipe: 'Wein' },
+    { name: 'La Croisade', recipe: 'Wein' },
+    { name: 'Grauburgunder', recipe: 'Wein' },
     { name: 'Elis & Tom', recipe: 'Cachaca 6cl, Maraschino 1 cl, Gommesirup (2 Schus oder 4cl), Aquafaba 6cl (2 Schuss), Coloca um cereija no fundo do copo e um pouco do sirup das cerejas, coloca crushed ice no copo (metade), coloca drink, depois coloca mais crushed ice e uma cereja com o sirup das cerejas para decorar' }
   ];
 
